eb7566c659551eefef790e8d04fb5a65ac62c5aa
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-02 18:56:37 -0400

    functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,10 @@
 
 # YOUR CODE HERE
 
+
+def is_even(n): return n % 2
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +13,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+print(["Even!", "Odd!"][is_even(num)])

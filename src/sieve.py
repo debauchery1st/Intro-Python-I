828e6d892b69d2760f2e9f7307eb81dbfce56bba
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-03 15:40:10 -0400

    refactored

--- src/sieve.py
+++ src/sieve.py
@@ -8,12 +8,14 @@ class Sieve(object):
     collection = None
 
     def __init__(self, n):
+        # start with a list of integers from 2 through n+1
         self.collection = [x for x in range(2, n+1)]
 
     def sift(self, customFilter=None):
         if customFilter is None:
             return
-        return list(filter(customFilter, self.collection))
+        # return a filtered list of unique elements
+        return list(set(filter(customFilter, self.collection)))
 
 
 class Eratosthenes(Sieve):
@@ -22,24 +24,31 @@ class Eratosthenes(Sieve):
     primes = []
 
     def __init__(self, n):
-        super().__init__(n)
-        self.stop = sqrt(n)
-        self.last_prime = self.collection[0]
-        self.run()
+        super().__init__(n)  # initialize Sieve
+        self.last_prime = self.collection[0]  # start with the first element
+        self.stop = sqrt(n)  # stop sifting at the square root of N
+        self.sortPrimes()  # run the sifting cycle, then sort the result.
 
     def next_prime(self):
         if (self.last_prime > self.stop):
+            # we are done sifting
             return False
+        # remove any numbers divisible by "last_prime"
         self.collection = self.sift(lambda x: x % self.last_prime != 0)
+        # store the list of primes we have sifted thus far
         self.primes.append(self.last_prime)
+        # remove any stored primes from the collection
+        self.collection = self.sift(lambda x: x not in self.primes)
+        # the first element of our sifted collection becomes the next prime
         self.last_prime = self.collection[0]
         return True
 
-    def run(self):
+    def sortPrimes(self):
         getPrime = True
         while getPrime:
-            getPrime = self.next_prime()
-        self.primes = sorted([x for x in set(self.primes + self.collection)])
+            getPrime = self.next_prime()  # get the next prime ?
+        # produce a sorted list of unique primes
+        self.primes = sorted(list(set(self.primes + self.collection)))
         return True
 
 

7da8f7ba3fb3a01d2b33460a8886a6052239cc99
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-03 03:42:39 -0400

    Stretch goal: The Sieve of Eratosthenes

---
+++ src/stretch.py
@@ -0,0 +1,46 @@
+from math import sqrt
+
+# Stretch goal: The Sieve of Eratosthenes
+
+
+class Sieve(object):
+    stop = None
+    collection = None
+
+    def __init__(self, n):
+        self.stop = sqrt(n)
+        self.collection = [x for x in range(2, n+1)]
+
+
+class Eratosthenes(Sieve):
+    last_prime = None
+    primes = []
+
+    def __init__(self, n):
+        super().__init__(n)
+        self.last_prime = self.collection[0]
+        self.run()
+
+    def next_prime(self):
+        if (self.last_prime > self.stop):
+            return False
+        self.collection = self.filterPowersOf(self.last_prime)
+        self.primes.append(self.last_prime)
+        self.last_prime = self.collection[0]
+        return True
+
+    def filterPowersOf(self, num):
+        def foo(bar): return bar % num != 0
+        return list(filter(foo, self.collection))
+
+    def run(self):
+        getPrime = True
+        while getPrime:
+            getPrime = self.next_prime()
+        self.primes += self.collection
+        self.collection = None
+        return True
+
+
+# print all prime numbers less than 2020
+print(Eratosthenes(2020).primes)

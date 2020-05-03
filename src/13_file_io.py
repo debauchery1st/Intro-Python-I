c0d8ce54ae00331aaa363c0f491c12bad77d143e
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-02 21:17:18 -0400

    file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,30 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt", "r") as f:
+    print(f.read())
 
-# Open up a file called "bar.txt" (which doesn't exist yet) for
-# writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make
-# sure that it contains what you expect it to contain
+    # Open up a file called "bar.txt" (which doesn't exist yet) for
+    # writing. Write three lines of arbitrary content to that file,
+    # then close the file. Open up "bar.txt" and inspect it to make
+    # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+    # YOUR CODE HERE
+text = """Quarantined leaping
+A brilliant lambda writes
+betrayed by the kiss
+"""
+
+print('\nwriting file... \n')
+
+with open("bar.txt", "w") as f:
+    f.write(text)
+f.close()
+
+print("\n... reading file\n")
+
+with open("bar.txt", "r") as f:
+    foo = f.read()
+f.close()
+
+print(f'[file content matches text: {foo == text}]\n\n{foo}')

69c2021acc19cfd4a218f546ffaac7728e6e8036
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-05 10:36:02 -0400

    absolute path to file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,15 +10,17 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open("foo.txt", "r") as f:
+from os import path
+absfile = path.join(path.dirname(path.abspath(__file__)), "foo.txt")
+with open(absfile, "r") as f:
     print(f.read())
+f.close()
+# Open up a file called "bar.txt" (which doesn't exist yet) for
+# writing. Write three lines of arbitrary content to that file,
+# then close the file. Open up "bar.txt" and inspect it to make
+# sure that it contains what you expect it to contain
 
-    # Open up a file called "bar.txt" (which doesn't exist yet) for
-    # writing. Write three lines of arbitrary content to that file,
-    # then close the file. Open up "bar.txt" and inspect it to make
-    # sure that it contains what you expect it to contain
-
-    # YOUR CODE HERE
+# YOUR CODE HERE
 text = """Quarantined leaping
 A brilliant lambda writes
 betrayed by the kiss
